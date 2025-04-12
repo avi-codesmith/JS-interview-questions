8307612ee2e8b070b9199bf7440ff6c57399daf1
avi-codesmith <avi.codesmith@gmail.com>
2025-04-12 18:10:29 +0530

    Coercion in JS

---
+++ Coercion/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+"use strict";
+
+console.log("Iam" + 23 + "yrs old");
+// Iam23yrs old
+
+const test = "23" + "2";
+console.log(test); // 232
+console.log(typeof test); // string
+
+const test2 = 1 + "1";
+console.log(test2); // 11
+console.log(typeof test2); // string
+
+const test3 = "1" - 1;
+console.log(test3); // 0
+console.log(typeof test3); // Number
+
+const test4 = 2 + 3 + 4 + "5";
+console.log(test4); // 95
+console.log(typeof test4); //string
